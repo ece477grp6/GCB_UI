1a00883fc18e1ad93133e753f531d40b61a01295
shen290 <shen290@purdue.edu>
2018-09-10 09:29:22 -0400

    update

--- GCB.py
+++ GCB.py
@@ -1,8 +1,11 @@
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-
+from qgmap.config import config
 from mainwindow import Ui_MainWindow
+from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkRequest
+from PyQt5.QtWebKit import QWebSettings
+from PyQt5.QtWebKitWidgets import QWebPage, QWebView
 
 class MainWindow(QMainWindow, Ui_MainWindow):
 
