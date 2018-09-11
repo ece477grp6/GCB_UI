6aa946ca13195b2f1fb5811d817ee8f6a5956c2d
shen290 <shen290@purdue.edu>
2018-09-10 21:19:54 -0400

    add map

--- GCB.py
+++ GCB.py
@@ -1,25 +1,13 @@
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-from mainwindow import Ui_MainWindow
-import sys
-sys.path.insert(0, "./qMap/")
-import qgmap # Retrived from https://github.com/eyllanesc/qMap
-from qgmap.common import QGoogleMap
+from mainwindow import *
 
-qgmap.use("PyQt5")
-
-
-class MainWindow(QMainWindow, Ui_MainWindow):
-
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-        self.setupUi(self)
-        self.show()
 
 
 if __name__ == '__main__':
 
     app = QApplication([])
     window = MainWindow()
+    window.show()
     app.exec_()
